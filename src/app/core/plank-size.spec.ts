30128379006e95fa7c9c9f37916a73034e755304
abcxys <abcxys92@gmail.com>
2024-07-23 23:44:54 -0400

    Add TypeScript interface for 'PlankSize' class.

---
+++ src/app/core/plank-size.spec.ts
@@ -0,0 +1,7 @@
+import { PlankSize } from './plank-size';
+
+describe('PlankSize', () => {
+  it('should create an instance', () => {
+    expect(new PlankSize()).toBeTruthy();
+  });
+});
---
+++ src/app/core/plank-size.ts
@@ -0,0 +1,12 @@
+export class PlankSize {
+  id: number;
+  widthInInch: string;
+  length: string;
+  thicknessInInch: string;
+  squarefootPerCarton: number;
+  createTime: Date;
+  updateTime: Date;
+  createUserId: number;
+  updatedUserId: number;
+  description: string;
+}
