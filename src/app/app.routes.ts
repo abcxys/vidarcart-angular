927cffef2298c7b7fbae8754dae9477d3f156530
abcxys <abcxys92@gmail.com>
2024-07-30 23:45:03 -0400

    Add routes for PlankSizeListComponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {PlankSizeListComponent} from "./plankSizeList/plank-size-list/plank-size-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "plankSizes", component: PlankSizeListComponent},
+  {path: "**", component: AppComponent}
+];
