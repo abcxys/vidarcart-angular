45f6551f24e26417b975b9b9807c2f5e1a811f70
abcxys <abcxys92@gmail.com>
2024-09-09 23:34:30 -0400

    Update auth-spring service with login method.

--- src/app/service/auth-spring.service.ts
+++ src/app/service/auth-spring.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthSpringService {
 
-  constructor() { }
+  private loginUrl: string = 'http://localhost:8080/login'; // Spring Security login endpoint.
+
+  constructor(private http: HttpClient) {}
+
+  public login(username: string, password: string): Observable<any> {
+    const body = new URLSearchParams();
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    return this.http.post(this.loginUrl, body.toString(), {
+      headers,
+      withCredentials: true // This will ensure that cookies (JSESSIONID) are stored and sent automatically
+    });
+  }
 }
